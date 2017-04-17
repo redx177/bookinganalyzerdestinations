1036c758dc37a1e8f08e9dce2c2a158a2ac3e78b
soultemptation <simonlangr@gmail.com>
2017-04-17 15:57:34 +0200

    changes

--- index.php
+++ index.php
@@ -8,7 +8,12 @@ $outputFP = fopen($output, 'w');
 $lineNames = fgetcsv($originalFP, 0, '@');
 fwrite($outputFP, implode('@', $lineNames) . "@place@region@country\n");
 $cache = [];
+$i = 0;
 while ($originalLine = fgetcsv($originalFP, 0, '@')) {
+    // Sleep for 10 seconds after 100 requests.
+    if ($i != 0 && $i % 100 == 0) {
+        sleep(10);
+    }
     if (count($originalLine)+2 == count($lineNames)) {
         $originalLine[] = 0;
         $originalLine[] = 0;
@@ -20,24 +25,30 @@ while ($originalLine = fgetcsv($originalFP, 0, '@')) {
     $region = '';
     $country = '';
     if (array_key_exists($nref, $cache)) {
-        $place = $namedOriginalLine['place'];
-        $region = $namedOriginalLine['region'];
-        $country = $namedOriginalLine['country'];
+        if ($cache[$nref] !== false) {
+            $place = $cache[$nref]['place'];
+            $region = $cache[$nref]['region'];
+            $country = $cache[$nref]['country'];
+        }
     } else {
-        $url = "http://xxx/api/accommodations?properties=id($nref)";
+        $i++;
+        echo $i;
+        $url = "http://search.interhome.com/api/accommodations?properties=id($nref)";
         $response = file_get_contents($url);
         $json = json_decode($response);
-        print_r($json);
-        if ($json->count) {
+        //print_r($json);
+        if (isset($json->count)) {
             $place = trim($json->accommodations[0]->destination->parent->parent->name);
             $region = trim($json->accommodations[0]->destination->parent->name);
             $country = trim($json->accommodations[0]->destination->name);
 
             $cache[$nref] = [
-                'place' => $namedOriginalLine['place'],
-                'region' => $namedOriginalLine['region'],
-                'country' => $namedOriginalLine['country'],
+                'place' => $place,
+                'region' => $region,
+                'country' => $country,
             ];
+        } else {
+            $cache[$nref] = false;
         }
     }
     $namedOriginalLine['place'] = $place;
@@ -48,8 +59,18 @@ while ($originalLine = fgetcsv($originalFP, 0, '@')) {
     fwrite($outputFP, $outputLine);
 }
 
+// Store destinations into a file.
 $destinations = __DIR__ . '/destinations.csv';
 $destinationsFP = fopen($destinations, 'w');
+$cache2 = [];
 foreach ($cache as $item) {
-    fwrite($destinationsFP, implode(';', $item) . "\n");
+    // Prevent empty lines.
+    if ($item !== false) {
+        $cacheKey = implode("", $item);
+        // Prevent duplicates.
+        if (!key_exists($cacheKey, $cache2)) {
+            fwrite($destinationsFP, implode(';', $item) . "\n");
+            $cache2[$cacheKey] = true;
+        }
+    }
 }
